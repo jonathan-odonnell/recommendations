3d5e3a55c4a496b0cf366ac7e0244fddd0762d68
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2024-12-22 20:52:44 +0000

    added coffee recommendations and stats views, urls and templates

--- coffee/views.py
+++ coffee/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def coffee_recommendations(request):
+    template = 'coffee_recommendations.html'
+    return render(request, template)
--- stats/views.py
+++ stats/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def coffee_stats(request):
+    template = 'stats.html'
+    return render(request, template)
