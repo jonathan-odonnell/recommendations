c7821a0844dd0b0d19d6bfc7f700d03d9195f3b4
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2024-12-24 22:54:53 +0000

    added coffee recommendations results view, url and template

--- coffee/urls.py
+++ coffee/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.coffee_recommendations, name='coffee_recommendations'),
+    path('results/', views.coffee_recommendations_results, name='coffee_recommendations_results'),
 ]
--- coffee/views.py
+++ coffee/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 
+
 def coffee_recommendations(request):
-    template = 'coffee_recommendations.html'
+    if request.GET:
+        template = 'coffee_recommendations.html'
+        return render(request, template)
+
+
+def coffee_recommendations_results(request):
+    template='coffee_recommendations_results.html'
     return render(request, template)
