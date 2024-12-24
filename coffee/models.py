13354c195e922675305042756ac6643c5bf4ba0c
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2024-12-24 21:11:31 +0000

    amended coffee quality model

--- coffee/models.py
+++ coffee/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class CoffeeQuality(models.Model)
+class CoffeeQuality(models.Model):
     
     class Meta:
         verbose_name_plural = "Coffee Quality"
@@ -12,15 +12,15 @@ class CoffeeQuality(models.Model)
     variety = models.CharField(max_length=254)
     colour = models.CharField(max_length=254)
     processing_method = models.CharField(max_length=254)
-    aroma = models.DecimalField(decimal_places=2, max=10, min=0)
-    flavour = models.DecimalField(decimal_places=2, max=10, min=0)
-    aftertaste = models.DecimalField(decimal_places=2, max=10, min=0)
-    acidity = models.DecimalField(decimal_places=2, max=10, min=0)
-    body = models.DecimalField(decimal_places=2, max=10, min=0)
-    balance = models.DecimalField(decimal_places=2, max=10, min=0)
-    unifirmity = models.DecimalField(decimal_places=2, max=10, min=0)
-    sweetness = models.DecimalField(decimal_places=2, max=10, min=0)
-    moisture = models.DecimalField(decimal_places=2, max=10, min=0)
+    aroma = models.DecimalField(decimal_places=2, max_digits=4)
+    flavour = models.DecimalField(decimal_places=2, max_digits=4)
+    aftertaste = models.DecimalField(decimal_places=2, max_digits=4)
+    acidity = models.DecimalField(decimal_places=2, max_digits=4)
+    body = models.DecimalField(decimal_places=2, max_digits=4)
+    balance = models.DecimalField(decimal_places=2, max_digits=4)
+    unifirmity = models.DecimalField(decimal_places=2, max_digits=4)
+    sweetness = models.DecimalField(decimal_places=2, max_digits=4)
+    moisture = models.DecimalField(decimal_places=2, max_digits=4)
 
     def __str__(self):
         return self.name
