aef4ccf9024156033da496fb3e9acc6b2c587bec
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2024-12-24 22:53:39 +0000

    amended coffee quality model

--- coffee/models.py
+++ coffee/models.py
@@ -6,21 +6,21 @@ class CoffeeQuality(models.Model):
     class Meta:
         verbose_name_plural = "Coffee Quality"
 
-    species = models.CharField(max_length=254)
-    country_of_origin = models.CharField(max_length=254)
-    year_of_harvest = models.CharField(max_length=254)
-    variety = models.CharField(max_length=254)
-    colour = models.CharField(max_length=254)
-    processing_method = models.CharField(max_length=254)
-    aroma = models.DecimalField(decimal_places=2, max_digits=4)
-    flavour = models.DecimalField(decimal_places=2, max_digits=4)
-    aftertaste = models.DecimalField(decimal_places=2, max_digits=4)
-    acidity = models.DecimalField(decimal_places=2, max_digits=4)
-    body = models.DecimalField(decimal_places=2, max_digits=4)
-    balance = models.DecimalField(decimal_places=2, max_digits=4)
-    unifirmity = models.DecimalField(decimal_places=2, max_digits=4)
-    sweetness = models.DecimalField(decimal_places=2, max_digits=4)
-    moisture = models.DecimalField(decimal_places=2, max_digits=4)
+    species = models.CharField(max_length=254, null=True, blank=True)
+    country = models.CharField(max_length=254, null=True, blank=True)
+    year = models.IntegerField(null=True, blank=True)
+    variety = models.CharField(max_length=254, null=True, blank=True)
+    colour = models.CharField(max_length=254, null=True, blank=True)
+    processing_method = models.CharField(max_length=254, null=True, blank=True)
+    aroma = models.DecimalField(decimal_places=2, max_digits=4, null=True, blank=True)
+    flavour = models.DecimalField(decimal_places=2, max_digits=4, null=True, blank=True)
+    aftertaste = models.DecimalField(decimal_places=2, max_digits=4, null=True, blank=True)
+    acidity = models.DecimalField(decimal_places=2, max_digits=4, null=True, blank=True)
+    body = models.DecimalField(decimal_places=2, max_digits=4, null=True, blank=True)
+    balance = models.DecimalField(decimal_places=2, max_digits=4, null=True, blank=True)
+    uniformity = models.DecimalField(decimal_places=2, max_digits=4, null=True, blank=True)
+    sweetness = models.DecimalField(decimal_places=2, max_digits=4, null=True, blank=True)
+    moisture = models.DecimalField(decimal_places=2, max_digits=4, null=True, blank=True)
 
     def __str__(self):
         return self.name
