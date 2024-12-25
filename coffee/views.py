85cee526598dff4c40dfc0b00b658507a2dd92c8
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2024-12-25 18:33:25 +0000

    amended coffee views and templates

--- coffee/views.py
+++ coffee/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
+from .models import CoffeeQuality
 
 
 def coffee_recommendations(request):
-    if request.GET:
+    if request.method == 'POST':
+        return
+    else:
         template = 'coffee_recommendations.html'
         return render(request, template)
 
 
 def coffee_recommendations_results(request):
+    recommendations = CoffeeQuality.objects.all()[:10]
     template='coffee_recommendations_results.html'
-    return render(request, template)
+    context = {'recommendations': recommendations}
+    return render(request, template, context)
