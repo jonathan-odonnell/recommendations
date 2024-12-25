342a7d8a421c2337181fa2adcbb3e90e06234cb5
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2024-12-25 22:32:17 +0000

    amended coffee quality form

--- coffee/forms.py
+++ coffee/forms.py
@@ -6,32 +6,17 @@ class CoffeeQualityForm(forms.ModelForm):
 
     class Meta:
         model = CoffeeQuality
-        fields = '__all__'
+        exclude = ('species', 'variety', 'year', 'colour', 'processing_method', 'country', 'uniformity', 'sweetness', 'moisture',)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = [('Low', 'Low'), ('Medium', 'Medium'), ('High', 'High'),]
-        species = CoffeeQuality.objects.exclude(species=None).distinct('species')
-        countries = CoffeeQuality.objects.exclude(country=None).distinct('country')
-        years = CoffeeQuality.objects.exclude(year=None).distinct('year')
-        varieties = CoffeeQuality.objects.exclude(variety=None).distinct('variety')
-        colours = CoffeeQuality.objects.exclude(colour=None).distinct('colour')
-        processing_methods = CoffeeQuality.objects.exclude(processing_method=None).exclude(processing_method='Other').distinct('processing_method')
-        self.fields['species'] = forms.ChoiceField(choices=[(i.species, i.species) for i in species])
-        self.fields['country'] = forms.ChoiceField(choices=[(i.country, i.country) for i in countries])
-        self.fields['year'] = forms.ChoiceField(choices=[(i.year, i.year) for i in years])
-        self.fields['variety'] = forms.ChoiceField(choices=[(i.variety, i.variety) for i in varieties])
-        self.fields['colour'] = forms.ChoiceField(choices=[(i.colour, i.colour) for i in colours])
-        self.fields['processing_method'] = forms.ChoiceField(choices=[(i.processing_method, i.processing_method) for i in processing_methods])
         self.fields['aroma'] = forms.ChoiceField(choices=categories)
         self.fields['flavour'] = forms.ChoiceField(choices=categories)
         self.fields['aftertaste'] = forms.ChoiceField(choices=categories)
         self.fields['acidity'] = forms.ChoiceField(choices=categories)
         self.fields['body'] = forms.ChoiceField(choices=categories)
         self.fields['balance'] = forms.ChoiceField(choices=categories)
-        self.fields['uniformity'] = forms.ChoiceField(choices=categories)
-        self.fields['sweetness'] = forms.ChoiceField(choices=categories)
-        self.fields['moisture'] = forms.ChoiceField(choices=categories)
 
         self.helper = FormHelper(self)
         self.helper.field_class = 'mb-3'
