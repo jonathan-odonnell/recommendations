8098a8f1ff0d5b73c7e4882bd62fa641c280f875
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2024-12-25 21:15:13 +0000

    Added logic to coffee recommendations and coffee recommendations results views

--- coffee/views.py
+++ coffee/views.py
@@ -1,20 +1,45 @@
 from django.shortcuts import render
 from .models import CoffeeQuality
 from .forms import CoffeeQualityForm
+import decimal
 
 
 def coffee_recommendations(request):
-    if request.method == 'POST':
-        return
-    else:
-        form = CoffeeQualityForm()
-        template = 'coffee_recommendations.html'
-        context = {'form': form}
-        return render(request, template, context)
+    form = CoffeeQualityForm()
+    template = 'coffee_recommendations.html'
+    context = {'form': form}
+    return render(request, template, context)
 
 
 def coffee_recommendations_results(request):
-    recommendations = CoffeeQuality.objects.all()[:10]
-    template='coffee_recommendations_results.html'
-    context = {'recommendations': recommendations}
-    return render(request, template, context)
+    if request.method == 'POST':
+        thresholds = {
+            'Low': '0.00_3.33',
+            'Medium': '3.34_6.66',
+            'High': '6.67_10.00',
+        }
+        recommendations = CoffeeQuality.objects.filter(
+            species=request.POST['species'],
+            country=request.POST['country'],
+            year=decimal(request.POST['species']),
+            variety=request.POST['colour'],
+            colour=request.POST['colour'],
+            processing_method=request.POST['processing_method'],
+            aroma=thresholds[request.post['aroma']],
+            flavour=thresholds[request.POST['flavour']],
+            aftertaste=thresholds[request.POST['aftertaste']],
+            acidity=thresholds[request.POST['acidity']],
+            body=thresholds[request.POST['body']],
+            balance=thresholds[request.POST['balance']],
+            uniformity=thresholds[request.POST['uniformity']],
+            sweetness=thresholds[request.POST['sweetness']],
+            moisture=thresholds[request.POST['moisture']],
+        )[:10]
+        template = 'coffee_recommendations_results.html'
+        context = {'recommendations': recommendations}
+        return render(request, template, context)
+    else:
+        recommendations = CoffeeQuality.objects.all()[:10]
+        template = 'coffee_recommendations_results.html'
+        context = {'recommendations': recommendations}
+        return render(request, template, context)
