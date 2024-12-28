774d3527404b289b6712664cd6b21ff4883bed46
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2024-12-28 18:33:45 +0000

    bug fixes to calculate averages correctly for  columns containing percentages and grams

--- stats/views.py
+++ stats/views.py
@@ -3,21 +3,28 @@ import pandas as pd
 
 
 def coffee_stats(request):
-    df = pd.read_csv('stats/fixtures/starbucks.csv')
-    beverages_value_count = df['Beverage_category'].value_counts()
-    nutrients = df.groupby('Beverage_category')['Calories'].mean().nlargest()
-    nutrients = nutrients.reset_index()
-    categories = list(df.columns)[3:]
     current_category = 'Calories'
 
     if request.GET and 'category' in request.GET:
         current_category = request.GET['category']
 
+    df = pd.read_csv('stats/fixtures/starbucks.csv')
+    
+    if pd.api.types.is_string_dtype(df.dtypes[current_category]):
+        df[current_category] = df[current_category].str.replace("%", '').astype(float)
+        print(df[current_category].mean())
+    
+    beverages_value_count = df['Beverage_category'].value_counts()
+    nutrients = df.groupby('Beverage_category')[current_category].mean().nlargest()
+    nutrients = nutrients.reset_index()
+    categories = list(df.columns)[3:]
+
+
     dataset = {
         'beverage_labels': beverages_value_count.index.to_list(),
         'beverage_data': beverages_value_count.to_list(),
         'nutrients_labels': nutrients['Beverage_category'].to_list(),
-        'nutrients_data': nutrients['Calories'].to_list()
+        'nutrients_data': nutrients[current_category].to_list()
     }
     template = 'stats.html'
     context = {
@@ -25,8 +32,8 @@ def coffee_stats(request):
         'current_category': current_category,
         'categories': categories,
         'count': df.count()['Beverage'],
-        'min': df['Calories'].min(),
-        'max': df['Calories'].max(),
-        'average': df['Calories'].mean(),
+        'min': df[current_category].min(),
+        'max': df[current_category].max(),
+        'average': df[current_category].mean(),
         }
     return render(request, template, context)
