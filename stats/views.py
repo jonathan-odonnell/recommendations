0a8bd56f24f56285eb13e8daabcb99516919edff
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2024-12-28 18:39:36 +0000

    Amended chart titles

--- stats/views.py
+++ stats/views.py
@@ -9,18 +9,17 @@ def coffee_stats(request):
         current_category = request.GET['category']
 
     df = pd.read_csv('stats/fixtures/starbucks.csv')
-    
+
     if pd.api.types.is_string_dtype(df.dtypes[current_category]):
         df[current_category] = df[current_category].str.replace("%", '').astype(float)
-        print(df[current_category].mean())
-    
+
     beverages_value_count = df['Beverage_category'].value_counts()
     nutrients = df.groupby('Beverage_category')[current_category].mean().nlargest()
     nutrients = nutrients.reset_index()
     categories = list(df.columns)[3:]
 
-
     dataset = {
+        'category': current_category,
         'beverage_labels': beverages_value_count.index.to_list(),
         'beverage_data': beverages_value_count.to_list(),
         'nutrients_labels': nutrients['Beverage_category'].to_list(),
