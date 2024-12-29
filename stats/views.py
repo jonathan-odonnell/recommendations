a56653eca76e301cc065beeed58a90373d6a11bf
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2024-12-29 18:10:56 +0000

    Added logic for scatter and doughnut charts in stats view and template

--- stats/views.py
+++ stats/views.py
@@ -10,7 +10,8 @@ def coffee_stats(request):
         current_category = request.GET['category']
 
     df = pd.DataFrame(list(Stat.objects.all().values()))
-    beverages_value_count = df['category'].value_counts()
+    beverage_categories_value_count = df['category'].value_counts()
+    beverages_value_count = pd.concat([df['name'].value_counts().head(5), df['name'].value_counts().tail(5)]).sort_index()
     nutrients = df.groupby('category')[current_category].mean()
     nutrients = nutrients.reset_index()
     categories = Nutrient.objects.order_by('id')
@@ -19,10 +20,14 @@ def coffee_stats(request):
     dataset = {
         'category': category.name,
         'unit': category.units,
-        'beverage_labels': beverages_value_count.index.to_list(),
-        'beverage_data': beverages_value_count.to_list(),
+        'beverage_categories_labels': beverage_categories_value_count.index.to_list(),
+        'beverages_data': beverages_value_count.to_list(),
+        'beverages_labels': beverages_value_count.index.to_list(),
+        'beverage_categories_data': beverage_categories_value_count.to_list(),
         'nutrients_labels': nutrients['category'].to_list(),
-        'nutrients_data': nutrients[current_category].to_list()
+        'nutrients_data': nutrients[current_category].to_list(),
+        'calories_sugar_labels': df['sugar'].to_list(),
+        'calories_sugar_data': df['calories'].to_list(),
     }
     template = 'stats.html'
     context = {
