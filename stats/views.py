7de40450760a42782af5165230ffd2e7fa5d9b14
jonathan-odonnell <jonathanodonnell1994@gmail.com>
2024-12-29 00:17:19 +0000

    Added database logic and refactored code in stats view and template

--- stats/views.py
+++ stats/views.py
@@ -1,36 +1,36 @@
 from django.shortcuts import render
 import pandas as pd
+from .models import Nutrient, Stat 
+from decimal import Decimal
 
 
 def coffee_stats(request):
-    current_category = 'Calories'
+    current_category = 'calories'
 
     if request.GET and 'category' in request.GET:
         current_category = request.GET['category']
 
-    df = pd.read_csv('stats/fixtures/starbucks.csv')
-
-    if pd.api.types.is_string_dtype(df.dtypes[current_category]):
-        df[current_category] = df[current_category].str.replace("%", '').astype(float)
-
-    beverages_value_count = df['Beverage_category'].value_counts()
-    nutrients = df.groupby('Beverage_category')[current_category].mean().nlargest()
+    df = pd.DataFrame(list(Stat.objects.all().values()))
+    beverages_value_count = df['category'].value_counts()
+    nutrients = df.groupby('category')[current_category].mean()
     nutrients = nutrients.reset_index()
-    categories = list(df.columns)[3:]
+    categories = Nutrient.objects.all()
+    category = Nutrient.objects.get(name__iexact=current_category.replace('_', ' '))
 
     dataset = {
-        'category': current_category,
+        'category': category.name,
+        'unit': category.units,
         'beverage_labels': beverages_value_count.index.to_list(),
         'beverage_data': beverages_value_count.to_list(),
-        'nutrients_labels': nutrients['Beverage_category'].to_list(),
+        'nutrients_labels': nutrients['category'].to_list(),
         'nutrients_data': nutrients[current_category].to_list()
     }
     template = 'stats.html'
     context = {
         'dataset': dataset,
-        'current_category': current_category,
+        'current_category': category,
         'categories': categories,
-        'count': df.count()['Beverage'],
+        'count': df.count()['name'],
         'min': df[current_category].min(),
         'max': df[current_category].max(),
         'average': df[current_category].mean(),
